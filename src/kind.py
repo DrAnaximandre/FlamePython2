3c1289e629b9d14b5175f6a3b8f5b65aba828ddd
DrAnaximandre <pool.lemaitre@gmail.com>
2024-03-26 18:41:56 -0400

    progress

--- src/kind.py
+++ src/kind.py
@@ -2,17 +2,30 @@ import numpy as np
 from LFOs.LFOSet import LFOSet
 from Variation import Variation
 
+@dataclass
+class FuntionMapping():
+
+    l: LFOSet = None
+    colors: list = None
+    wights: list = None
+    A: np.ndarray = None
+
+    def __post_init__(self):
+        self.populate()
+
+    def populate(self):
+        self.functions = []
+        for i in range(6):
+            self.functions.append(Function([1], self.A[i], [self.l[i]]))
+
+
 
 @dataclass
 class VariationFactory:
 
-    l: LFOSet = None
+    
     N: int = 25000
-    colors: list = None
-    A: np.ndarray = None
-.   variation: Variation = None
-    functions = FunctionMapping()
-
+    func_map: FunctionMapping
 
     def __post_init__(self):
         self.populate()
@@ -20,6 +33,6 @@ class VariationFactory:
     def populate(self):
    
         variation = Variation(N)
-        for f in self.functions:
-            variation.addFunction(f.weights, f.params, f.additives, f.relative_proba, f.color)
+        for i, f in enumerate(self.functions):
+            variation.addFunction(, f.params, f.additives, f.relative_proba, f.color)
         variation.addFinal([1], np.array([0,0.99,0,0,0,0.99]), [raton])
