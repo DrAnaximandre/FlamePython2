3f4236dac09e3b28f81005fb41ac4d3a16e5bc33
polo <pool.lemaitre@gmail.com>
2024-03-31 09:20:51 -0400

    refactor more

--- src/kind.py
+++ src/kind.py
@@ -1,7 +1,6 @@
 import numpy as np
 from LFOs.LFOSet import LFOSet
 from dataclasses import dataclass
-from Variation import Variation
 from colors import Color
 import numpy as np
 
@@ -21,19 +20,18 @@ class FunctionMapping():
     def __post_init__(self):
         assert self.Ax.shape == self.Ay.shape
         assert self.Ax.shape[1] == 3
-        assert self.Ax.shape[0] == len(self.weights)+1 # assuming the last function is the final
-        assert len(self.weights)+1 == len(self.additives)
-        assert len(self.colors) == len(self.weights)
-        assert len(self.probabilites) == len(self.weights)
+        assert self.Ax.shape[0] == len(self.weights) # assuming the last function is the final
+        assert len(self.weights) == len(self.additives)
+        assert len(self.colors) == len(self.weights)-1 # the final has no color
+        assert len(self.probabilites) == len(self.weights)-1 # the final has no probability
         self.construct()
 
     def call(self, coordinates):
 
         N_loc = coordinates.shape[0]  # how many points in the batch
         r = np.random.uniform(size=N_loc)  # each point is attributed a rand
-        res = np.zeros(shape=(N_loc, 5))  # creation of the empty results x y r g b
-        coordinates = np.concatenate((coordinates, 255 * np.ones((N_loc, 3))), axis=1)
-    
+        result = np.zeros(shape=(N_loc, 5))  # creation of the empty results x y r g b
+        
         for i in range(len(self.relative_probabilites)-1):  # for each function
             # we select via a mask the points that are attributed a given function
             mask_1 = r > self.relative_probabilites[i]
@@ -41,13 +39,13 @@ class FunctionMapping():
             selection = np.where((mask_1) & (mask_2))[0]
 
             # function call
-            res[selection, :] = self.apply_function(i, coordinates[selection, :])
+            result[selection, :] = self.apply_function(i, coordinates[selection, :])
         
         # final call
-        res= self.apply_function(i, coordinates)
+        result = self.apply_final(result)
         
 
-        return res
+        return result
  
     def construct_probas(self):
         self.relative_probabilites = [0] + [sum(self.probabilites[:i+1]) for i in range(len(self.probabilites))]
@@ -55,36 +53,49 @@ class FunctionMapping():
 
     def construct(self):
         self.construct_probas()
-       
-    def apply_function(self, i, coordinates):
+    
+    @staticmethod
+    def build_points(coordinates):
         N_points = coordinates.shape[0]
         intercepts = np.ones((N_points, 1))
         points = np.concatenate((intercepts, coordinates[:,:2]), axis=1)
+
+        return points
+
+    def apply_function(self, i, coordinates):
+       
+        points = self.build_points(coordinates)
         x_loc = np.dot(points, self.Ax[i,:])
         y_loc = np.dot(points, self.Ay[i,:])
-        
-        result_loc = np.zeros((N_points, 5))
+        result_loc = np.zeros((points.shape[0], 5))
 
         for j in range(len(self.weights[i])):
-            
             result_loc[:,:2] += self.weights[i][j] * self.additives[i][j](x_loc, y_loc)
         
         for j in range(3):
-            if i != len(self.colors)-1:
-                
-                result_loc[:, j+2] = self.colors[i][j] + coordinates[:, j+2]
-
-
+            result_loc[:, j+2] = self.colors[i][j]/2 + coordinates[:, j+2]/2
+           
         return result_loc
         
+    def apply_final(self, result):
+
+        points = self.build_points(result)
+        x_loc = np.dot(points, self.Ax[-1,:])
+        y_loc = np.dot(points, self.Ay[-1,:])
+
+        for j in range(len(self.weights[-1])):
+            result[:,:2] += self.weights[-1][j] * self.additives[-1][j](x_loc, y_loc)
+        
+        return result
+
 
 if __name__ == "__main__":
 
-    l = LFOSet()
+    l = LFOSet(0.9)
     c = Color()
     colors = [c.R[3], c.G[3], c.B[2]]
    
-    weights = [[0.5, 0.2], [0.5], [0.1,0.001,0.4]]
+    weights = [[0.5, 0.2], [0.5], [0.1,0.001,0.4],[0.3]]
     additives = [[linear, swirl], [bubble], [linear, swirl, spherical], [linear]]
     Ax = np.array([[1,0,l.alpha0],[1,1,0],[1,0,1],[1,l.alpha1,1]])
     Ay = np.array([[-1,0,l.alpha2],[1,-1,0],[1,0,1],[1,l.alpha3,1]])
@@ -92,6 +103,62 @@ if __name__ == "__main__":
 
     fm = FunctionMapping(l, colors, weights, additives, Ax, Ay, probabilites)
 
-    fm.call(np.random.rand(250000,2))
 
+    l2 = LFOSet(0.5)
+    weights = [[0.2, 0.5], [0.5], [0.21,0.2001,0.24],[0.23]]
+    additives = [[linear, swirl], [bubble], [linear, swirl, spherical], [linear]]
+    Ax = np.array([[1,0,l.alpha0],[1,1,0],[1,0,1],[1,l.alpha1,1]])
+    Ay = np.array([[-1,0,l.alpha2],[1,-1,0],[1,0,1],[1,l.alpha3,1]])
+    probabilites = [0.2, 0.3, 0.5]
+    fm2 = FunctionMapping(l, colors, weights, additives, Ax, Ay, probabilites)
 
+    @dataclass
+    class VH():
+        list_of_function_mappings: list
+        burn_steps: int
+        iterate_steps: int
+        N: int
+
+        def __post_init__(self):
+            self.total_N = self.N*len(self.list_of_function_mappings)
+
+        def run(self):
+            initial_coordinates  = self.burn()
+            result = self.iterate(initial_coordinates)
+            return result
+
+        def burn(self):
+            
+            initial_coordinates = np.random.uniform(-1,1, (self.total_N, 2))
+            # it is the first time the coordinates are called, attach a color
+            initial_coordinates = np.concatenate((initial_coordinates, 255 * np.ones((self.total_N, 3))), axis=1)
+    
+            for i in range(self.burn_steps):
+                for j, fm in enumerate(self.list_of_function_mappings):
+                    initial_coordinates[j*self.N:(j+1)*self.N,:] = fm.call(initial_coordinates[j*self.N:(j+1)*self.N,:])
+
+            return initial_coordinates
+
+        def iterate(self, initial_coordinates):
+                
+                result = np.zeros((self.total_N*self.iterate_steps, 5))
+
+                for i in range(self.iterate_steps):
+                    if i==0:
+                        result[:self.total_N,:] = initial_coordinates
+                    else:
+                        result[i*self.total_N:(i+1)*self.total_N,:] = result[(i-1)*self.total_N:(i)*self.total_N,:]
+
+                    for j, fm in enumerate(self.list_of_function_mappings):
+                        result[i*self.total_N:(i+1)*self.total_N] = fm.call(result[i*self.total_N:(i+1)*self.total_N])
+                       
+                return result
+
+        def to_image(self, result):
+
+            pass
+
+    vh = VH([fm, fm2],10,15, 2500)
+
+    result = vh.run()
+    print(result)
