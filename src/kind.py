ad60bd86f3e5b97ba62865b70ff0f488c7f67c03
polo <pool.lemaitre@gmail.com>
2024-03-31 21:52:57 -0400

    clean repo

--- src/kind.py
+++ src/kind.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 from colors import Color
 import numpy as np
 from PIL import Image
-from Additives import spherical, linear, bubble, swirl, expinj, sinmoche, pdj, raton
+from Additives import spherical, linear, bubble, swirl, expinj, sinmoche, pdj
 from numba import njit
 from moviepy.editor import *
 
@@ -205,7 +205,7 @@ def do_video_with_image_from_parameters(size=512):
         size=size,
     ) for i in range(n_im + 1)]
 
-    Parallel(n_jobs=-2)(
+    Parallel(n_jobs=-3)(
         delayed(images_to_generate[i].run)() for i in range(n_im + 1)
     )
 
@@ -236,17 +236,27 @@ class ImageHolder():
 
         l = LFOSet(ratio=self.ratio)
         c = Color()
-        colors = [c.B[0], c.B[3], c.B[2]]
+        colors = [c.B[0], c.B[3], c.RNP[2]]
     
         weights = [[0.55], [0.5,-0.2], [0.5,0.1],[1, 0.2]]
         additives = [[linear], [linear,bubble], [linear, spherical], [linear, bubble]]
-        Ax = np.array([[0,1,0],[1,1,0],[0,1,0],[0,1,0]])
-        Ay = np.array([[0,0,1],[0,0,1],[1,0,1],[0,0,1]])
+        Ax = np.array([[-l.alpha2*0.05,1,l.alpha0/3],[1,1,0],[-l.alpha1*0.05,1,l.alpha2*0.05],[0,1,l.alpha1/10]])
+        Ay = np.array([[l.alpha2/10,l.alpha3/10,1],[0,l.alpha3/20,1],[1,l.alpha0/10,1],[-l.alpha2/10,0,1]])
         probabilites = np.array([0.1, 0.1, 0.1])
         
-        fm = FunctionMapping(l, colors, weights, additives, Ax, Ay, probabilites, final=False)
+        fm = FunctionMapping(l, colors, weights, additives, Ax, Ay, probabilites, final=True)
 
-        self.vh = VH([fm],15,30, 5000)
+        colors = [c.B[0], c.R[3], c.R[0]]
+        weights = [[0.55], [0.5,-0.2], [0.5,0.1],[1, 0.2]]
+        additives = [[linear], [linear,bubble], [linear, spherical], [linear, bubble]]
+        Ax = np.array([[-1+l.alpha2*0.05,1,l.alpha0/3],[1,1,0],[0,1,0],[0,1,l.alpha1/10]])
+        Ay = np.array([[l.alpha2/10,l.alpha3/10,1],[-1,l.alpha3/20,1],[1,l.alpha0/10,1],[-l.alpha2/10,0,1]])
+        probabilites = np.array([0.1, 0.1, 0.1])
+        
+        fm2 = FunctionMapping(l, colors, weights, additives, Ax, Ay, probabilites, final=True)
+
+
+        self.vh = VH([fm, fm2],15,30, 150000)
 
     def create_folder(self, folder_name):
         if not os.path.exists(folder_name):
@@ -259,15 +269,5 @@ class ImageHolder():
 
 if __name__ == "__main__":
 
-    #do_video_with_image_from_parameters()
-    from time import time
-    times = []
-    for i in range(20):
-        t0 = time()
-        ih = ImageHolder(i)
-        ih.run()
-        t1 = time()
-        times.append(t1-t0)
-
-    print(f"Mean time: {np.mean(times)}")
-       
+    
+    do_video_with_image_from_parameters(666)
